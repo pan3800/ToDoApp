f5c2661438c36b0f2c5b65d74e2a8ba62a64a3e7
pan3800 <pan173434@gmail.com>
2024-07-02 12:17:06 +0900

    ToDoApp 프로젝트 생성

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by 임채성 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
