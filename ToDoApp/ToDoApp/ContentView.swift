9f279dedf128b68381fa1303acfc96c2e30889fe
pan3800 <pan173434@gmail.com>
2024-07-04 13:16:15 +0900

    Todo 완료기능(if문)구현

--- ToDoApp/ToDoApp/ContentView.swift
+++ ToDoApp/ToDoApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var todoList: [Todo] = [
+    @State var todoList: [Todo] = [
         Todo(title: "친구 만나기"),
         Todo(title: "과제 제출하기"),
         Todo(title: "푹쉬기")
@@ -22,12 +22,29 @@ struct ContentView: View {
                 
                 ForEach(todoList) { todo in
                     HStack {
-                        Image(systemName: "circle")
-                            .foregroundStyle(Color.pink)
+                        
+                        if todo.isCompleted == true {
+                            //투두가 완료 되었을때
+                            Image(systemName: "circle.fill")
+                                .foregroundStyle(Color.pink)
+                                .onTapGesture {
+                                    todo.isCompleted.toggle()
+                                }
+                            
+                        } else {
+                            //투두가 완료되지 않았을때
+                            Image(systemName: "circle")
+                                .foregroundStyle(Color.pink)
+                                .onTapGesture {
+                                    todo.isCompleted.toggle()
+                                }
+                        }
+                        
                         NavigationLink {
                             Text("다음 화면입니다.")
                         } label: {
                             Text(todo.title)
+                                .strikethrough(todo.isCompleted, color: Color.gray)
                         }
                     }
                 }
--- ToDoApp/ToDoApp/Todo.swift
+++ ToDoApp/ToDoApp/Todo.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 class Todo: Identifiable {
     let id: UUID
     var title: String
