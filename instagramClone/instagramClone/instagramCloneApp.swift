dfc592312128307c66447132fa38f0977f5d70e1
pan3800 <pan173434@gmail.com>
2024-07-18 16:18:00 +0900

    Firebase 세팅하기

--- instagramClone/instagramClone/instagramCloneApp.swift
+++ instagramClone/instagramClone/instagramCloneApp.swift
@@ -5,10 +5,24 @@
 //  Created by 임채성 on 2024/07/10.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct instagramCloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
