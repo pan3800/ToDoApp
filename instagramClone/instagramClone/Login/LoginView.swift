f14f655e5c785ac3e649195c71e758fd8144c1ec
pan3800 <pan173434@gmail.com>
2024-07-22 14:30:15 +0900

    로그인뷰에 그라디언트 뷰 넣기

--- instagramClone/instagramClone/Login/LoginView.swift
+++ instagramClone/instagramClone/Login/LoginView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    let yellowColor = Color(red: 0.9960784314, green: 0.9764705882, blue: 0.9529411765)
+    let redColor = Color(red: 0.9921568627, green: 0.9490196078, blue: 0.9725490196)
+    let blueColor = Color(red: 0.9333333, green: 0.968627451, blue: 0.9960784314)
+    let greenColor = Color(red: 0.937254902, green: 0.9882352941, blue: 0.9529411765)
+    
     var body: some View {
-//        LinearGradient(colors: [Color.red, Color.blue], startPoint: .top, endPoint: .bottom)
         LinearGradient(stops: [
-            Gradient.Stop(color: .red, location: 0.1),
-            Gradient.Stop(color: .blue, location: 0.8),
+            Gradient.Stop(color: yellowColor, location: 0.1),
+            Gradient.Stop(color: redColor, location: 0.3),
+            Gradient.Stop(color: blueColor, location: 0.6),
+            Gradient.Stop(color: greenColor, location: 1),
         ], startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+
     }
 }
 
