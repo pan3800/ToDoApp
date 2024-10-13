1bbb57da04456f64f056aac78d3e1873893cb373
pan3800 <pan173434@gmail.com>
2024-10-13 15:20:25 +0900

    검색결과 클릭해서 프로필뷰로 이동 구현

--- instagramClone/instagramClone/Feed/Search/SearchView.swift
+++ instagramClone/instagramClone/Feed/Search/SearchView.swift
@@ -26,27 +26,30 @@ struct SearchView: View {
         NavigationStack {
             List {
                 ForEach(filteredUsers) { user in
-                    HStack {
-                        if let imageUrl = user.profileImageUrl {
-                            KFImage(URL(string: imageUrl))
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 53, height: 53)
-                                .clipShape(Circle())
-                        } else {
-                            Image(systemName: "person.circle.fill")
-                                .resizable()
-                                .frame(width: 53, height: 53)
-                                .opacity(0.5)
-                        }
-                        VStack(alignment: .leading) {
-                            Text(user.username)
-                            Text(user.bie ?? "")
-                                .foregroundStyle(.gray)
+                    NavigationLink {
+                        ProfileView(viewModel: ProfileViewModel(user: user))
+                    } label: {
+                        HStack {
+                            if let imageUrl = user.profileImageUrl {
+                                KFImage(URL(string: imageUrl))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 53, height: 53)
+                                    .clipShape(Circle())
+                            } else {
+                                Image(systemName: "person.circle.fill")
+                                    .resizable()
+                                    .frame(width: 53, height: 53)
+                                    .opacity(0.5)
+                            }
+                            VStack(alignment: .leading) {
+                                Text(user.username)
+                                Text(user.bie ?? "")
+                                    .foregroundStyle(.gray)
+                            }
                         }
                     }
                     .listRowSeparator(.hidden)
-                    
                 }
             }
             .listStyle(.plain)
